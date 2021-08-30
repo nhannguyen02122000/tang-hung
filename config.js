b9bbd79d3cfeb0003c62400ae69d3fb9862cb383
nhannguyen02122000 <nhan.nguyen02122000@hcmut.edu.vn>
2021-08-30 21:27:47 +0700

    init

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Hey Việt Hùng!',
+    introDesc: `Cậu biết gì không
+    Tôi là một khách hàng khó tính đấy
+    Biết sao không?
+    Vì toi yeu cau rat nhieu :>`,
+    btnIntro: 'Hị Hị',
+    title: 'Chán quá nên làm cái web này, cậu nói đi...',
+    desc: 'Cậu thích toi chứ giề ;)))',
+    btnYes: 'Cóooo <33',
+    btnNo: 'Không, chia buồn :3',
+    question:'Một tí lý do nào :>',
+    btnReply: 'Gửi cho người có aura thánh thiện',
+    reply: 'Iu Nhân nhiều <3',
+    mess: 'Tôi biết mà :> :> :>',
+    messDesc: 'Yêu thương toi nhiều hơn nhé',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/tannyan.nguyen'
+}
